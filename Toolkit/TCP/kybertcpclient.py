625bf78270beb305283dd3d44a600601225ec7c2
Alex Kollar <cythesout@gmail.com>
2022-08-12 02:51:55 -0400

    Added clear screen and spruced up the TCP Stuff.

--- Toolkit/TCP/kybertcpclient.py
+++ Toolkit/TCP/kybertcpclient.py
@@ -1,13 +1,19 @@
 import socket
 print("""
- ______________    ________          __ 
-/_  __/ ___/ _ \  / ___/ (_)__ ___  / /_
- / / / /__/ ___/ / /__/ / / -_) _ \/ __/
-/_/  \___/_/     \___/_/_/\__/_//_/\__/ 
-            @cythesout
+                   __ __      __                         
+                  / //_/_  __/ /_  ___  _____            
+                 / ,< / / / / __ \/ _ \/ ___/            
+                / /| / /_/ / /_/ /  __/ /                
+               /_/ |_\__, /_.___/\___/_/                 
+  ________________  /____/_____    ___________   ________
+ /_  __/ ____/ __ \   / ____/ /   /  _/ ____/ | / /_  __/
+  / / / /   / /_/ /  / /   / /    / // __/ /  |/ / / /   
+ / / / /___/ ____/  / /___/ /____/ // /___/ /|  / / /    
+/_/  \____/_/       \____/_____/___/_____/_/ |_/ /_/    
+                  @ssgcythes
       """)
-targetHost = input("What is the host (url or ip) you want to connect to:\n")
-targetPort = int(input("What is the port number:\n"))
+targetHost = input("Host IP or URL: ")
+targetPort = int(input("Host port: "))
 #create a socket object:
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #connect to the client:
--- Toolkit/TCP/kybertcpserv.py
+++ Toolkit/TCP/kybertcpserv.py
@@ -1,17 +1,38 @@
-import socket, threading
-print("""
- ______________    ____                    
-/_  __/ ___/ _ \  / __/__ _____  _____ ____
- / / / /__/ ___/ _\ \/ -_) __/ |/ / -_) __/
-/_/  \___/_/    /___/\__/_/  |___/\__/_/ 
-""")
+import socket, threading, time
+from os import system, name
+sleep = time.sleep(3.0)
+banner ="""
+                   __ __      __                          
+                  / //_/_  __/ /_  ___  _____             
+                 / ,< / / / / __ \/ _ \/ ___/             
+                / /| / /_/ / /_/ /  __/ /                 
+               /_/ |_\__, /_.___/\___/_/                  
+  ________________  /____/__ __________ _    ____________ 
+ /_  __/ ____/ __ \   / ___// ____/ __ \ |  / / ____/ __ \\
+  / / / /   / /_/ /   \__ \/ __/ / /_/ / | / / __/ / /_/ /
+ / / / /___/ ____/   ___/ / /___/ _, _/| |/ / /___/ _, _/ 
+/_/  \____/_/       /____/_____/_/ |_| |___/_____/_/ |_|  
+"""
+
+def clearScreen():
+        # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+
+print(banner)
 servIp = input("Enter an IP for your server: ")
 servPort = int(input("Enter a port for the server: "))
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((servIp, servPort)) 
     server.listen(5)
-    print(f"[*] Listning on {servIp}:{servPort}")
+    clearScreen()
+    print(banner)
+    print(f"\n[*] Listning on {servIp}:{servPort}")
     while True:
         client, address  = server.accept()
         print(f"[*] Accepted connection from {address[0]}:{address[1]}")
